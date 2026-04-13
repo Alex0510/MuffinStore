c96a0a946b2106181163466733013935220dde4e
Mr.Eric <Alex0510@users.noreply.github.com>
2026-04-13 09:51:11 +0800

    更新 MFSRootViewController.h

--- MFSRootViewController.h
+++ MFSRootViewController.h
@@ -1,6 +1,8 @@
+// MFSRootViewController.h
 #import <UIKit/UIKit.h>
 #import <Preferences/PSListController.h>
 #import <Preferences/PSSpecifier.h>
+#import <objc/runtime.h>
 
 @interface MFSRootViewController : PSListController
 
